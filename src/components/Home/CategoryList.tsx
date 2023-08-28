65f0106ac4aba04c92cdb4c8dfe778a6b3a8efaf
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-08-29 00:10:08 +0900

    refactor: apply responsive style

--- src/components/Home/CategoryList.tsx
+++ src/components/Home/CategoryList.tsx
@@ -1,6 +1,6 @@
 import CategoryListData from "@/Shared/Data";
 import Image from "next/image";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
 interface Category {
   id: number;
@@ -11,24 +11,45 @@ interface Category {
 
 const CategoryList = () => {
   const categoryList = CategoryListData;
+  const [isOpen, setIsOpen] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState<
     Category["name"] | null
   >(null);
+  const toggleIsOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      if (window.innerWidth < 768) {
+        setIsOpen(false);
+      } else {
+        setIsOpen(true);
+      }
+    }
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
   return (
     <div>
-      <h2 className="font-bold px-2 text-white">Selected Category</h2>
+      <div className="flex justify-space px-2">
+        <h2 className="font-bold text-white ">Selected Category</h2>
+        <div className="text-white md:hidden lg:hidden" onClick={toggleIsOpen}>
+          {isOpen ? "▲" : "▼"}
+        </div>
+      </div>
       <div
         className="grid 
         grid-cols-2 
-        
         md:grid-cols-2 
         lg:grid-cols-3"
       >
-        {categoryList.map((category, index) => (
-          <div
-            key={category.id}
-            className={`flex flex-col
+        {isOpen &&
+          categoryList.map((category, index) => (
+            <div
+              key={category.id}
+              className={`flex flex-col
                 justify-center items-center bg-black
                 p-2 m-2 rounded-lg grayscale 
                 border-[1px]
@@ -40,16 +61,16 @@ const CategoryList = () => {
                     ? "bg-white border-[1px]"
                     : null
                 }`}
-            onClick={() => setSelectedCategory(category.name)}
-          >
-            <Image
-              src={category.icon}
-              alt={category.name}
-              width={100}
-              height={100}
-            />
-          </div>
-        ))}
+              onClick={() => setSelectedCategory(category.name)}
+            >
+              <Image
+                src={category.icon}
+                alt={category.name}
+                width={100}
+                height={100}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
